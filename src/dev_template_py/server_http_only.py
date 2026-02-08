f3967e226252ed8a2e6cc3918c6885720a3226bd
Werner Paulin <72560449+wernerpaulin@users.noreply.github.com>
2026-02-08 19:05:22 +0100

    MQTT additionally implemented

---
+++ src/dev_template_py/server_http_only.py
@@ -0,0 +1,35 @@
+"""A small example server that handles post requests on /add."""
+import logging
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from dev_template_py.example import add_numbers
+
+class BelowErrorFilter(logging.Filter):
+    """A logging filter that only allows loggings below level ERROR."""
+
+    def filter(self, record):
+        """Filter everything with loggings below level ERROR."""
+        return record.levelno < logging.ERROR
+
+
+logger = logging.getLogger(__name__)
+
+
+class AddParameters(BaseModel):
+    """The parameters required in the body on /add."""
+
+    left: float
+    right: float
+
+
+app = FastAPI(title="MyApp API", version="1.0")
+
+
+@app.post("/add")
+async def handle_post_add(params: AddParameters):
+    """Handles a request on /add by adding the two number from the body."""
+    logger.info("/add endpoint accessed")
+    result = add_numbers(params.left, params.right)
+    return {"result": result}
